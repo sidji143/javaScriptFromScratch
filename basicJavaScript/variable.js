1ba77385fed006f446babb47c389f7d2459887cf
sidji143 <57484393+sidji143@users.noreply.github.com>
2025-12-30 10:46:33 +0000

    Understanding Variable in javaScript

---
+++ basicJavaScript/variable.js
@@ -0,0 +1,7 @@
+let accountId =464646
+let accountMailId ="adbs@gmail.com"
+var accountPassword="hasjhsd"
+accountCity="Gurgaon"
+let accountState
+
+console.table([accountId,accountMailId,accountPassword,accountCity,accountState])
