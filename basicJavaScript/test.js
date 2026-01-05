485dbc4c44c6d1bf6cdfaf8eb44ccfa040876611
sidji143 <57484393+sidji143@users.noreply.github.com>
2026-01-05 16:09:44 +0000

    Stack and Heap in the javaScript

--- basicJavaScript/test.js
+++ basicJavaScript/test.js
@@ -1,1 +1,13 @@
 console.log("Siddharth ojha");
+
+
+// Stack and Heap in the javaScript
+
+//  Every primitive value goes to stack and not primitive value goes to heap
+//  For exaple 
+// let nameOne ="javaScriptOne"
+// let nameTwo = nameOne
+
+// let userOne ={
+//     email="userOne@gmail.com"
+// }
